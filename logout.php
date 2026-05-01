c5ba9fa453b67b5fa3b660be7aaed4bb9ca90943
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-05-01 17:39:43 +0500

    add new logout code

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset(); // Tamam variables khatam karein
-session_destroy(); // Session khatam karein
+session_unset();
+session_destroy();
 header("Location: login.php");
 exit();
 ?>
