f776bd0c4262a97000fc9a91c4d089a443d4ffeb
Ahmedkhn143 <ahmadkha8143@gmail.com>
2026-05-14 07:58:01 +0500

    remove footer section

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,5 @@
     </div> <!-- .main-content -->
 </div> <!-- .wrapper -->
 
-<footer style="text-align:center; padding: 20px 0; background: #f4f4f4; color: #888; margin-top: 40px;">
-    &copy; <?php echo date('Y'); ?> EduPortal. All rights reserved.
-</footer>
-
 </body>
 </html>
